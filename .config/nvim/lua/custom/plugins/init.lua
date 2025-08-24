97d8c4b94e57194f9f357e8926b8f4f6adba852a
ZackNyx <91201105+ZackNyx@users.noreply.github.com>
2025-08-24 23:19:41 +0400

    nvim: Add indent lines plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -65,4 +65,16 @@ return {
   {
     '42paris/42header',
   },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+      indent = {
+        char = '▏',
+      },
+      scope = { enabled = false },
+    },
+  },
 }
