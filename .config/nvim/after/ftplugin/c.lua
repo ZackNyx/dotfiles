5b0eb08c7db982e5c0e6e0f3bb9c438f4aff0bec
ZackNyx <91201105+ZackNyx@users.noreply.github.com>
2026-01-09 00:16:08 +0400

    nvim: Add comments to C filetype plugin

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -1,5 +1,8 @@
 local set = vim.cmd
 
+-- sets the width of indentation to 4 chars
 set 'setlocal shiftwidth=4'
+-- sets the visual width of a tabulation to 4 chars
 set 'setlocal tabstop=4'
+-- makes it so that spaces are used instead of tabulations
 set 'setlocal expandtab'
